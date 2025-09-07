6c09092674210d4cf54f36eb33c8051d74e29680
junma168 <3060383143@qq.com>
2025-09-07 17:57:23 +0800

    注释掉base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/docs-vitepress/",
+  // base: "/docs-vitepress/",
   head: [["link", { rel: "icon", href: "/logo.png" }]],
   title: "我的学习笔记",
   description: "A VitePress Site",
